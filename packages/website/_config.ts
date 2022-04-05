1bd9f10fdc0d3dc4c1c220eee156b215188fbc6c
xhyrom <generalkubo@gmail.com>
2022-04-05 20:40:29 +0200

    new things

--- packages/website/_config.ts
+++ packages/website/_config.ts
@@ -1,7 +1,6 @@
 import lume from 'https://deno.land/x/lume@v1.7.2/mod.ts';
 import postcss from 'https://deno.land/x/lume@v1.7.2/plugins/postcss.ts';
 import codeHighlight from 'https://deno.land/x/lume@v1.7.2/plugins/code_highlight.ts';
-import inline from 'https://deno.land/x/lume@v1.7.2/plugins/inline.ts';
 import resolveUrls from 'https://deno.land/x/lume@v1.7.2/plugins/resolve_urls.ts';
 import esbuild from 'https://deno.land/x/lume@v1.7.2/plugins/esbuild.ts';
 import date from 'https://deno.land/x/lume@v1.7.2/plugins/date.ts';
@@ -11,7 +10,7 @@ const site = lume(
     src: './src',
     server: {
       page404: './404/',
-    }
+    },
   },
 );
 
@@ -19,9 +18,9 @@ site
   .ignore('README.md')
   .ignore('scripts')
   .copy('info.txt', './info.txt')
+  .copy('static', './static')
   .use(resolveUrls())
   .use(codeHighlight())
-  .use(inline())
   .use(postcss())
   .use(date())
   .use(esbuild({
@@ -31,6 +30,6 @@ site
     (path) => path.endsWith('.css'),
     (path) => path.endsWith('.png') || path.endsWith('.jpg'),
   )
-  .filter('slice', (arr, length) => arr.slice(0, length))
+  .filter('slice', (arr, length) => arr.slice(0, length));
 
 export default site;
--- packages/website/src/blog/posts/first.md
+++
@@ -1,8 +0,0 @@
----
-title: This is my first post
-description: This is a post on My Blog about leveraging agile frameworks.
-date: 2022-04-05
----
-
-Lorem impus sola dulea> lol  
-> lul
