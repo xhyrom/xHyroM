736b00999508bc8c9202d3c7c2247788b61e9479
xhyrom <generalkubo@gmail.com>
2022-04-09 13:32:36 +0200

    functions

--- packages/site/src/functions/add_post.ts
+++ packages/site/src/functions/add_post.ts
@@ -1,12 +1,7 @@
 // @ts-nocheck
 
-export async function onRequestGet({ env }) {
-    const res = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
-    const data = await res.json();
-    const info = JSON.stringify(data);
-
+export async function onRequestGet() {
     return new Response(JSON.stringify({
-        test: typeof admin_name,
-        info
+        test: true
     }));
 }
