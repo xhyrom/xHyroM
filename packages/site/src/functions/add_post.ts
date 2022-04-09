43da019deee45423d09fe439a72041df92b87c23
xhyrom <generalkubo@gmail.com>
2022-04-09 13:36:35 +0200

    add post

--- packages/site/src/functions/add_post.ts
+++ packages/site/src/functions/add_post.ts
@@ -2,6 +2,6 @@
 
 export async function onRequestGet() {
     return new Response(JSON.stringify({
-        test: true
+        test: typeof admin_name === 'undefined' ? 'nothing' : admin_name
     }));
 }
