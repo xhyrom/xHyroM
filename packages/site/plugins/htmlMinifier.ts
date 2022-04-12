940dcf6d7809e3023fb4716d2118c4cf80d7bbb8
xhyrom <generalkubo@gmail.com>
2022-04-12 21:19:30 +0200

    new lume version

--- packages/site/plugins/htmlMinifier.ts
+++ packages/site/plugins/htmlMinifier.ts
@@ -1,4 +1,4 @@
-import type { Site } from "https://deno.land/x/lume@v1.7.2/core.ts";
+import type { Site } from "https://deno.land/x/lume@v1.7.3/core.ts";
 import { Language, minify } from "https://deno.land/x/minifier@v1.1.1/mod.ts";
 
 export const htmlMinifier = () => {
