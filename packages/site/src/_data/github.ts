6ce68d52db49fd584dc1568016757c1d882e231d
xhyrom <generalkubo@gmail.com>
2022-04-17 16:46:36 +0200

    branches

--- packages/site/src/_data/github.ts
+++ packages/site/src/_data/github.ts
@@ -1,17 +1,21 @@
+interface Branch {
+  name: string;
+}
+
 interface Repository {
   id: number;
   name: string;
+  owner: string;
+  last_update: number;
   stats: {
     license: string | null;
     size: number;
     fork: "yes" | "no";
   };
+  branches: Branch[];
   description: string | null;
   url: string;
 }
-
-const repositories =
-  await (await fetch("https://api.github.com/users/xHyroM/repos")).json();
 
 export const formatBytes = (bytes: number, decimals = 2) => {
   if (bytes === 0) return "0 Bytes";
@@ -25,18 +29,35 @@ export const formatBytes = (bytes: number, decimals = 2) => {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 };
 
-export default {
-  repositories: repositories.map((repository: any) =>
+const repositories = (await (await fetch("https://api.github.com/users/xHyroM/repos")).json()).map((repository: any) =>
+  new Object({
+    id: repository.id,
+    owner: repository.owner.login,
+    name: repository.name,
+    last_update: new Date(repository.updated_at).getTime(),
+    stats: {
+      size: formatBytes(repository.size * 1000),
+      license: repository?.license?.spdx_id ?? "?",
+      fork: repository.fork ? "yes" : "no",
+    },
+    description: repository.description,
+    url: repository.svn_url,
+  })
+) as Repository[];
+
+for (const repository of repositories) {
+  const branches = await (await fetch(`https://api.github.com/repos/xHyroM/${repository.name}/branches`)).json();
+
+  repository.branches = branches.map((branch: Branch) => 
     new Object({
-      id: repository.id,
-      name: repository.name,
-      stats: {
-        size: formatBytes(repository.size * 1000),
-        license: repository?.license?.spdx_id ?? "?",
-        fork: repository.fork ? "yes" : "no",
-      },
-      description: repository.description,
-      url: repository.svn_url,
+      name: branch.name,
+      url: `https://github.com/${repository.owner}/${repository.name}/tree/${branch.name}/`
     })
-  ) as Repository[],
+  )
+}
+
+repositories.sort((a, b) => b.last_update - a.last_update)
+
+export default {
+  repositories: repositories,
 };
