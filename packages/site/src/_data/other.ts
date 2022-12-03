257b4c7d64290eb8d6f3dacf5bedb3567b026517
Hyro <generalkubo@gmail.com>
2022-12-03 19:19:34 +0100

    Update other.ts

--- packages/site/src/_data/other.ts
+++ packages/site/src/_data/other.ts
@@ -1,4 +1,4 @@
-const applications = await (await fetch('https://raw.githubusercontent.com/xHyroM/discord-activities/master/activities.json')).json()
+const applications = await (await fetch('https://raw.githubusercontent.com/xHyroM/discord-list/master/activities/activities.json')).json()
 
 export default {
     appIds: applications.map((app: any) => `"${app.id}"`)
